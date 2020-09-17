da07faed15c462699ad8b1eb2dfd5c84ec4d2e51
JCON3DEV <jconheasman@gmail.com>
2020-09-17 19:23:00 +0000

    added functionality to the admin controller to show the table.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   private
   def show
+    @product_count = Product.count 
+    @categories_count = Product.group(:category_id).count
   end
+
 end
