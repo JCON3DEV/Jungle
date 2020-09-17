5b06812813b0f557742ff60bca69e6d6a58fd90e
JCON3DEV <jconheasman@gmail.com>
2020-09-17 19:45:07 +0000

    corrected the counts for admin views. All displays and works as expected

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
-  private
-  def show
+  
+  def show    
     @product_count = Product.count 
-    @categories_count = Product.group(:category_id).count
+    @categories_count = Category.count
   end
 
 end
