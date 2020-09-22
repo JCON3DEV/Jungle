75af90434d247b0acf9b545515c747f11e6910b4
JCON3DEV <jconheasman@gmail.com>
2020-09-22 04:22:51 +0000

    added test for email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,13 +27,19 @@ RSpec.describe User, type: :model do
       @user = User.new(first_name: "Bert", last_name: "Reynolds", email: "madeup@gmail.com", password: nil, password_confirmation: nil)
       expect(@user.save).to be(false)
     end
-
+    
+    it "is not valid if multiple users have the same email regardless of case" do
+      @user1 = User.new(first_name: "Bert", last_name: "Reynolds", email: "madeup@gmail.com", password: "123", password_confirmation: "123")
+      expect(@user1.save).to be true
+      @user2 = User.new(first_name: "Bert", last_name: "Reynolds", email: "MADEUP@gmail.com", password: "123", password_confirmation: "123")
+      expect(@user2.save).to be false
+    end
+    
     it "is not valid if password is shorter than 3 characters" do
       @user = User.new(first_name: "Bert", last_name: "Reynolds", email: "madeup@gmail.com", password: "123", password_confirmation: "123")
       expect(@user.password.length).to be_between(3,20).inclusive
     end
-    
-  
+
   end
 
 end
